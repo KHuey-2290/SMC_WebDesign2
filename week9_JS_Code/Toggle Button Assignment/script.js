7ca66d9848a49062aebd0a7d54d4a2daf1885e18
KHuey-2290 <huey_karissa_yayoi@student.smc.edu>
2020-05-11 15:51:47 -0700

    JS_Code (C5): JS added working JS to images to change images with corresponding buttons

--- week9_JS_Code/Toggle Button Assignment/script.js
+++ week9_JS_Code/Toggle Button Assignment/script.js
@@ -34,6 +34,71 @@ var headerColorChange = function(){
     })   
 }
 
+var swapButtercupImage = function(){
+    console.log(flowerButtercupImage.alt);
+
+    if(flowerButtercupImage.alt == "First image of California ButterCup"){
+        flowerButtercupImage.src = "Assets/buttercup1.jpg";
+        flowerButtercupImage.alt = "Second image of California ButterCups";
+        flowerButtercupImage.innerText = "Show the first one!";
+    } 
+    else {
+        flowerButtercupImage.src = "Assets/buttercup2.jpg";
+        flowerButtercupImage.alt = "First image of California ButterCups";
+        flowerButtercupImage.innerText = "Show the second one!";
+    }
+}
+
+var swapDaisyImage = function(){
+    console.log(flowerDaisyImage.alt);
+
+    if (flowerDaisyImage.alt == "First image of Fleabane Daisies"){
+        flowerDaisyImage.src = "Assets/daisy1.jpg";
+        flowerDaisyImage.alt = "Second image of Fleabane Daisies";
+        flowerDaisyImage.innerText = "Show the first one!";
+    } 
+    else  {
+        flowerDaisyImage.src = "Assets/daisy2.jpg";
+        flowerDaisyImage.alt = "First image of Fleabane Daisies";
+        flowerDaisyImage.innerText = "Show the second one!";
+    }
+}
+
+var swapLilyImage = function(){
+    console.log(flowerLilyImage.alt);
+
+    if(flowerLilyImage.alt == "First image of Catalina Mariposa Lilies"){ 
+        flowerLilyImage.src = "Assets/lily1.jpg";
+        flowerLilyImage.alt = "Second image of Catalina Mariposa Lilies";
+        flowerLilyImage.innerText = "Show the first one!";
+    } 
+    else { 
+        flowerLilyImage.src = "Assets/lily2.jpg";
+        flowerLilyImage.alt = "First image of Catalina Mariposa Lilies";
+        flowerLilyImage.innerText = "Show the second one!";
+    }
+}
+
+var swapPoppyImage = function(){
+    console.log(flowerPoppyImage.alt);
+
+    if(flowerPoppyImage.alt == "First image of California Poppies"){ 
+        flowerPoppyImage.src = "Assets/poppy1.jpg";
+        flowerPoppyImage.alt = "Second image of California Poppies";
+        flowerPoppyImage.innerText = "Show the first one!";
+    } 
+    else { 
+        flowerPoppyImage.src = "Assets/poppy2.jpg";
+        flowerPoppyImage.alt = "First image of California Poppies";
+        flowerPoppyImage.innerText = "Show the second one!";
+    }
+}
+
 headerColorChange();
 
 headerButton.addEventListener("click", headerColorChange);
+
+flowerButtercupButton.addEventListener("click", swapButtercupImage);
+flowerDaisyButton.addEventListener("click", swapDaisyImage);
+flowerLilyButton.addEventListener("click", swapLilyImage);
+flowerPoppyButton.addEventListener("click", swapPoppyImage);
