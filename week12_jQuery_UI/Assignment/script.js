5b3e5f42455a22f26dc91f95a7a26277dc9995f6
KHuey-2290 <huey_karissa_yayoi@student.smc.edu>
2020-05-31 23:22:39 -0700

    jQuery UI (C2): set up HTML, CSS, and JS files; built basic html and js structure for foundation of webpage

---
+++ week12_jQuery_UI/Assignment/script.js
@@ -0,0 +1,39 @@
+// autocomplete widget
+$( function() {
+    var availableTags = [
+        "ActionScript",
+        "AppleScript",
+        "Asp",
+        "BASIC",
+        "C",
+        "C++",
+        "Clojure",
+        "COBOL",
+        "ColdFusion",
+        "Erlang",
+        "Fortran",
+        "Groovy",
+        "Haskell",
+        "Java",
+        "JavaScript",
+        "Lisp",
+        "Perl",
+        "PHP",
+        "Python",
+        "Ruby",
+        "Scala",
+        "Scheme"
+    ];
+
+    $( "#tags" ).autocomplete({
+        source: availableTags
+    });
+} );
+
+// accordion widget
+$( function() {
+    $( "#accordion" ).accordion({
+        heightStyle: "content",
+        collapsible: true
+    });
+} );
