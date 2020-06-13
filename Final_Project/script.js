1e9c1797d384b8a98dfb448f3919f281cc6f6867
KHuey-2290 <huey_karissa_yayoi@student.smc.edu>
2020-06-13 01:02:35 -0700

    Final Project (C4): hopefully finished RESPONSIVE NAVBAR for html, css, and js; adjusted images according to layout and color; chose a google font for website "Open Sans"

---
+++ Final_Project/script.js
@@ -0,0 +1,17 @@
+var myNavLinks = document.getElementById("myFlexNavbarLinks");
+var mySMIconsBox = document.getElementById("mySMIconsBox");
+
+myNavLinks.style.display = "none";
+mySMIconsBox.style.display = "none";
+
+
+$(document).ready(function () {
+    //Buttons in header
+    $("#buttonSMToggle").click(function() {
+        $("#mySMIconsBox").toggle();
+    });
+
+    $("#buttonNavbarToggle").click(function() {
+        $("#myFlexNavbarLinks").toggle();
+    });
+})
