a53113bdb18a72b3ce02115529ed64120bb3e164
KHuey-2290 <huey_karissa_yayoi@student.smc.edu>
2020-06-13 02:56:07 -0700

    Final Project (C5): cannot figure out how to build on responsive navbar for larger screens; finished with mobile view; added height restraint for navbar

--- Final_Project/script.js
+++ Final_Project/script.js
@@ -1,9 +1,9 @@
+// Navbar JS
 var myNavLinks = document.getElementById("myFlexNavbarLinks");
 var mySMIconsBox = document.getElementById("mySMIconsBox");
 
 myNavLinks.style.display = "none";
 mySMIconsBox.style.display = "none";
-
 
 $(document).ready(function () {
     //Buttons in header
@@ -14,4 +14,4 @@ $(document).ready(function () {
     $("#buttonNavbarToggle").click(function() {
         $("#myFlexNavbarLinks").toggle();
     });
-})
+});
