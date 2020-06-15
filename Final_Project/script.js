4cd22a6900312cdcf04a32cd0828f184bbcd5196
KHuey-2290 <huey_karissa_yayoi@student.smc.edu>
2020-06-14 19:01:33 -0700

    FInal Project (C6): attempted to fix navbar problem

--- Final_Project/script.js
+++ Final_Project/script.js
@@ -1,9 +1,4 @@
 // Navbar JS
-var myNavLinks = document.getElementById("myFlexNavbarLinks");
-var mySMIconsBox = document.getElementById("mySMIconsBox");
-
-myNavLinks.style.display = "none";
-mySMIconsBox.style.display = "none";
 
 $(document).ready(function () {
     //Buttons in header
