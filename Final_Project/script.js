48f2500bb522c6da246e5527fcaeccdbbf95b36b
KHuey-2290 <huey_karissa_yayoi@student.smc.edu>
2020-06-15 18:44:40 -0700

    Final Project (C9): almost complete with all responsive pages for smallest screen size. Donation page still needs to be worked on

--- Final_Project/script.js
+++ Final_Project/script.js
@@ -1,5 +1,4 @@
 // Navbar JS
-
 $(document).ready(function () {
     //Buttons in header
     $("#buttonSMToggle").click(function() {
@@ -10,3 +9,33 @@ $(document).ready(function () {
         $("#myFlexNavbarLinks").toggle();
     });
 });
+
+// Home Page Slideshow
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var mySlides = document.getElementsByClassName("slides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > mySlides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = mySlides.length}
+    for (i = 0; i < mySlides.length; i++) {
+        mySlides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    mySlides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+}
